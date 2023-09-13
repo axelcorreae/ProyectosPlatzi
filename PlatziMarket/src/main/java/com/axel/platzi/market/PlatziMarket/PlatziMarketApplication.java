b99fc82d0387067d4acab14a45689e5f640b6e79
Axel Correa <axelcorrea@Axels-MacBook-Pro.local>
2023-09-13 00:00:38 -0500

    primer commit

---
+++ PlatziMarket/src/main/java/com/axel/platzi/market/PlatziMarket/PlatziMarketApplication.java
@@ -0,0 +1,13 @@
+package com.axel.platzi.market.PlatziMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziMarketApplication.class, args);
+	}
+
+}
---
+++ PlatziMarket/src/test/java/com/axel/platzi/market/PlatziMarket/PlatziMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axel.platzi.market.PlatziMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
